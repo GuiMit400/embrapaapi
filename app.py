adc1a72589796e46389aac0a9c4e6845f369b075
Mitsuo <60677695+GuiMit400@users.noreply.github.com>
2024-11-04 23:47:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -204,3 +204,5 @@ def get_exportacao(subopt, ano):
 
 if __name__ == '__main__':
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Obtém a porta da variável de ambiente, com fallback para 5000
+    app.run(host="0.0.0.0", port=port) 
